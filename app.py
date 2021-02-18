2e99a14fb47a439adbec42b3af6f35c9ec6cde59
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-18 16:55:27 +0000

    Change POST request to GET

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app.config["DEBUG"] = True
 app.config["JSON_SORT_KEYS"] = False
 
 
-@app.route('/classify', methods=['POST'])
+@app.route('/classify', methods=['GET'])
 def classify_utterance():
     try:
         body = request.get_json()
